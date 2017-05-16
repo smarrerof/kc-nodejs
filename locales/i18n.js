acf86792753f4ead9d032edeb94a2a9c9a309507
Sergio Marrero <smarrerof@gmail.com>
2017-05-16 21:26:08 +0100

    Added comments to i18n module

--- locales/i18n.js
+++ locales/i18n.js
@@ -2,14 +2,37 @@
 
 const translations = require('./translations.json');
 
+// Idioma usado por defecto si no se obtiene una traducción para 
+// la key/language usados en el método translate
 const defaultLanguage = 'en';
 let requestLanguage;
 
+
+/**
+ * Configura el módulo extrayendo el idioma del header del request.
+ * 
+ * @param {any} req 
+ * @param {any} res 
+ * @param {any} next 
+ */
 function configure(req, res, next) {
   requestLanguage = req.headers.language;  
   next();
 }
 
+
+/**
+ * Obtiene una traducción para un idioma dado.
+ * Si no se pasa un idioma, se usa el idioma obtenido de la request. Si
+ * tampoco hay idioma en el request se usa defaultLanguage. Si no se obtiene
+ * una traducción para el par key/language se trata de obtener una traducción
+ * para el par key/defaultLanguage. Si esto también falla se devuelve la key
+ * como traducción.
+ * 
+ * @param {any} key 
+ * @param {any} language 
+ * @returns {string} translation
+ */
 function translate(key, language) {
   language = language || requestLanguage || defaultLanguage;
   console.log('tranlate to', language);
