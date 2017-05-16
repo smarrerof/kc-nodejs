cc7a752bb99218a9ab44652274515547b61f2d76
Sergio Marrero <smarrerof@gmail.com>
2017-05-16 16:58:23 +0100

    Clean up

--- app.js
+++ app.js
@@ -2,13 +2,10 @@
 
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
+//var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
-//var index = require('./routes/index');
-//var users = require('./routes/users');
 
 require('./lib/connectMongoose');
 require('./models/Ad');
@@ -28,9 +25,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-//app.use('/', index);
-//app.use('/users', users);
-
 // Add routes
 app.use('/apiv1/ads', require('./routes/apiv1/ads'));
 app.use('/apiv1/users', require('./routes/apiv1/users'));
@@ -44,7 +38,7 @@ app.use(function(req, res, next) {
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function(err, req, res) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
