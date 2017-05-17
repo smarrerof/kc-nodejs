fa061f250e72134a3de1da1202154671af87c359
Sergio Marrero <smarrerof@gmail.com>
2017-05-17 21:24:38 +0100

    Change start meaning in ads filter

--- models/ad.js
+++ models/ad.js
@@ -12,7 +12,7 @@ const adSchema = mongoose.Schema({
 adSchema.statics.findByFilter = function (filters, options, callback) {
   console.log('Filters', filters);
   mongoose.model('Ad').find(filters, callback)     
-    .skip(options.start * options.limit)
+    .skip(options.start)
     .limit(options.limit);
 };
 
